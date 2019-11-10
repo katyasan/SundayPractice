874da24c8a00135343a01fa83ff364d7995a974a
katyasan <katyasan007@gmail.com>
2019-11-10 12:07:34 -0600

    test case 6 added

---
+++ src/testCase6.java
@@ -0,0 +1,7 @@
+public class testCase6 {
+    public static void main(String[] args) {
+        System.out.println("first");
+        System.out.println("second");
+        System.out.println("third");
+    }
+}
